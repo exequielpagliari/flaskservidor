11151410f35ac17e0dc7d3404a308cc286775c5a
Exequiel Pagliari <58015928+exequielpagliari@users.noreply.github.com>
2023-06-25 20:31:15 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask ,jsonify ,request
+# del modulo flask importar la clase Flask y los métodos jsonify,request
+from flask_cors import CORS # del modulo flask_cors importar CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+app=Flask(__name__) # crear el objeto app de la clase Flask
+CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
+# configuro la base de datos, con el nombre el usuario y la clave
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'
+
+# URI de la BBDD driver de la BD user:clave@URLBBDD/nombreBBDD
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
+db= SQLAlchemy(app) #crea el objeto db de la clase SQLAlquemy
+ma=Marshmallow(app) #crea el objeto ma de de la clase Marshmallow
+# defino la tabla
+class Producto(db.Model): # la clase Producto hereda de db.Model
+    id=db.Column(db.Integer, primary_key=True) #define los campos de la tabla
+    nombre=db.Column(db.String(100))
+    precio=db.Column(db.Integer)
+    stock=db.Column(db.Integer)
+    imagen=db.Column(db.String(400))
+    def __init__(self,nombre,precio,stock,imagen): #crea el constructor de la clase
+        self.nombre=nombre # no hace falta el id porque lo crea sola mysql por ser auto_incremento
+        self.precio=precio
+        self.stock=stock
+        self.imagen=imagen
+
+# si hay que crear mas tablas , se hace aqui
+
+with app.app_context():
+    db.create_all() # aqui crea todas las tablas
+# ************************************************************
+class ProductoSchema(ma.Schema):
+    class Meta:
+        fields=('id','nombre','precio','stock','imagen')
+
+producto_schema=ProductoSchema() # El objeto producto_schema es para traer un producto
+productos_schema=ProductoSchema(many=True) # El objeto productos_schema es para traer multiples registros de producto
+
+# crea los endpoint o rutas (json)
+@app.route('/productos',methods=['GET'])
+def get_Productos():
+    all_productos=Producto.query.all() # el metodo query.all() lo hereda de db.Model
+    result=productos_schema.dump(all_productos) # el metodo dump() lo hereda de ma.schema y
+
+# trae todos los registros de la tabla return jsonify(result) # retorna un JSON de todos los registros de la tabla
+
+@app.route('/productos/<id>',methods=['GET'])
+def get_producto(id):
+    producto=Producto.query.get(id)
+    return producto_schema.jsonify(producto) # retorna el JSON de un producto recibido como parametro
