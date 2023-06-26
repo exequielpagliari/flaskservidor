fd7dd9d8fd803c1b28787568a4cf79af48c6f363
Exequiel Pagliari <58015928+exequielpagliari@users.noreply.github.com>
2023-06-25 22:33:09 -0300

    Servidor Funcionando

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_marshmallow import Marshmallow
 app=Flask(__name__) # crear el objeto app de la clase Flask
 CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
 # configuro la base de datos, con el nombre el usuario y la clave
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:Mariela@localhost:3308/proyecto'
 
 # URI de la BBDD driver de la BD user:clave@URLBBDD/nombreBBDD
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
@@ -42,6 +42,8 @@ productos_schema=ProductoSchema(many=True) # El objeto productos_schema es para 
 def get_Productos():
     all_productos=Producto.query.all() # el metodo query.all() lo hereda de db.Model
     result=productos_schema.dump(all_productos) # el metodo dump() lo hereda de ma.schema y
+    # trae todos los registros de la tabla
+    return jsonify(result) # retorna un JSON de todos los registros de la tabla
 
 # trae todos los registros de la tabla return jsonify(result) # retorna un JSON de todos los registros de la tabla
 
@@ -49,3 +51,43 @@ def get_Productos():
 def get_producto(id):
     producto=Producto.query.get(id)
     return producto_schema.jsonify(producto) # retorna el JSON de un producto recibido como parametro
+
+@app.route('/productos/<id>',methods=['DELETE'])
+def delete_producto(id):
+    producto=Producto.query.get(id)
+    db.session.delete(producto)
+    db.session.commit()
+    return producto_schema.jsonify(producto) # me devuelve un json con el registro eliminado
+
+@app.route('/productos', methods=['POST']) # crea ruta o endpoint
+def create_producto():
+#print(request.json) # request.json contiene el json que envio el cliente
+    nombre=request.json['nombre']
+    precio=request.json['precio']
+    stock=request.json['stock']
+    imagen=request.json['imagen']
+    new_producto=Producto(nombre,precio,stock,imagen)
+    db.session.add(new_producto)
+    db.session.commit()
+    return producto_schema.jsonify(new_producto)
+
+@app.route('/productos/<id>' ,methods=['PUT'])
+def update_producto(id):
+    producto=Producto.query.get(id)
+    producto.nombre=request.json['nombre']
+    producto.precio=request.json['precio']
+    producto.stock=request.json['stock']
+    producto.imagen=request.json['imagen']
+    db.session.commit()
+    return producto_schema.jsonify(producto)
+
+@app.route('/data', methods=['GET'])
+def get_query_string():
+    print(request.query_string)
+    return request.query_string
+
+
+
+# programa principal *******************************
+if __name__=='__main__':
+    app.run(debug=True, port=5000) # ejecuta el servidor Flask en el puerto 5000
